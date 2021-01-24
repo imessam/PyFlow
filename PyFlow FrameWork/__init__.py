95d243ed248ffb1a806d702f3778889b198c60be
Mohamed Essam Abdelfatah <31731234+imessam@users.noreply.github.com>
2021-01-24 22:35:50 +0200

    Delete PyFlow FrameWork directory

--- PyFlow FrameWork/__init__.py
+++
@@ -1,3 +0,0 @@
-import PyFlow.Activations,PyFlow.Backward,PyFlow.DataLoader,PyFlow.Layers,PyFlow.Losses,PyFlow.Optimizers,PyFlow.Models,PyFlow.Metrics
-print("Module imported")
-__all__ = ["Activations", "Backward", "DataLoader","Layers","Losses","Optimizers","Models","Metrics"]
